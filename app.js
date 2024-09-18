9446a2d5cfd8275eb6ab29758defac5c2c211b02
Naga bindu <145024323+Nagabindu95@users.noreply.github.com>
2024-09-18 22:50:12 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const cookiesParser = require("cookie-parser");
+const app = express();
+const router = require("./src/routes");
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(cors());
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+
+app.use(express.static(`${__dirname}/public`));
+app.use(cookiesParser());
+app.use("/", router);
+
+const PORT = process.env.PORT || 8080;
+
+// mongoose.set("useFindAndModify", false);
+async function connectDB() {
+  try {
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      autoIndex: true,
+    });
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection FAILED ", error);
+    process.exit(1);
+  }
+}
+connectDB();
+
+const connection = mongoose.connection;
+connection.once("open", function () {
+  console.log("MongoDB connection established successfully.");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(PORT, function () {
+  console.log("Server is running on port : ", PORT);
 });
 
 module.exports = app;
---
+++ constants.js
@@ -0,0 +1,5 @@
+// module.exports = {
+//     DB_NAME: "alumniConnectdb",
+//   };
+export const DB_NAME="alumniConnectdb"
+  
